a50b6ad01f333038f7f4248bdaa57ababb675c31
Your Name <muhdsyahrulnizam123@gmail.com>
2019-08-09 21:10:47 +0800

    added contact page

--- frontend/src/views/Contact.vue
+++ frontend/src/views/Contact.vue
@@ -1,9 +1,23 @@
 <template>
 	<v-app>
 		<Header></Header>
-		<v-container grid-list-xs>
-			<v-layout wrap>
-				<v-flex xs>
+		<v-container bg fill-height grid-list-md text-xs-center text-center>
+			<v-layout row wrap align-center>
+				<v-flex xs4>
+					<a href="//www.linkedin.com/in/syahrul-nizam-41b984135/">
+					<v-icon
+					x-large>fab fa-linkedin-in
+					</v-icon></a>
+				</v-flex>
+				<v-flex xs4>
+					<a href="mailto:syahrul.nizam@u.nus.edu">
+					<v-icon
+					x-large>fas fa-envelope</v-icon></a>
+				</v-flex>
+				<v-flex xs4>
+					<a href="//github.com/syahrul12345">
+					<v-icon
+					x-large>fab fa-github</v-icon></a>
 					
 				</v-flex>
 			</v-layout>
