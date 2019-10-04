31b0df04f792049333e9737ab0326c4a2b0b1c00
Your Name <muhdsyahrulnizam123@gmail.com>
2019-10-04 12:55:59 +0800

    deploy

---
+++ precache-manifest.fa4f170e74c91c4790c98b3b7eb9fbc4.js
@@ -0,0 +1,254 @@
+self.__precacheManifest = [
+  {
+    "revision": "a30f35ee9492418e9783551579c47fe9",
+    "url": "/Portfolio/img/neo.a30f35ee.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Portfolio/robots.txt"
+  },
+  {
+    "revision": "43c9c18f73c1e5a8bbc2b8e8fa1443fb",
+    "url": "/Portfolio/img/3.43c9c18f.png"
+  },
+  {
+    "revision": "ff3a23b6d0b1acae53e0",
+    "url": "/Portfolio/css/chunk-vendors.63be62ce.css"
+  },
+  {
+    "revision": "bd52a727b5449dc3f8195b72c9c58341",
+    "url": "/Portfolio/fonts/fa-regular-400.bd52a727.woff2"
+  },
+  {
+    "revision": "7ab2cb050690f93994c46258d49af5b6",
+    "url": "/Portfolio/fonts/fa-regular-400.7ab2cb05.woff"
+  },
+  {
+    "revision": "a03df7ab0ffc96cabbfe3dc71c60baaa",
+    "url": "/Portfolio/fonts/fa-regular-400.a03df7ab.eot"
+  },
+  {
+    "revision": "2cd8d991e82712f1a5c5de8ee869ca74",
+    "url": "/Portfolio/fonts/fa-regular-400.2cd8d991.ttf"
+  },
+  {
+    "revision": "2cd2be177470d5096992572176bbe76e",
+    "url": "/Portfolio/fonts/fa-solid-900.2cd2be17.woff2"
+  },
+  {
+    "revision": "f861a57c52ef711cf807a3eec92c0e17",
+    "url": "/Portfolio/fonts/fa-brands-400.f861a57c.woff2"
+  },
+  {
+    "revision": "457cb96b6191ed105c4fe8463957f70d",
+    "url": "/Portfolio/fonts/fa-brands-400.457cb96b.woff"
+  },
+  {
+    "revision": "657a469469a2fc38a2901c68a3d56512",
+    "url": "/Portfolio/fonts/fa-solid-900.657a4694.woff"
+  },
+  {
+    "revision": "3165b14bbf3b64fca65829ecde6b800d",
+    "url": "/Portfolio/fonts/fa-brands-400.3165b14b.eot"
+  },
+  {
+    "revision": "f2e186cfab4787d4ef6f1bb192aa9a1b",
+    "url": "/Portfolio/fonts/fa-brands-400.f2e186cf.ttf"
+  },
+  {
+    "revision": "a547e21eceadf53602caf057be9ad9fd",
+    "url": "/Portfolio/fonts/fa-solid-900.a547e21e.eot"
+  },
+  {
+    "revision": "5ee32f5c8598e1a63ddf1aad4ffe54f4",
+    "url": "/Portfolio/fonts/fa-solid-900.5ee32f5c.ttf"
+  },
+  {
+    "revision": "2fad4ee0a47f41bc76e21c2baf613182",
+    "url": "/Portfolio/img/fa-regular-400.2fad4ee0.svg"
+  },
+  {
+    "revision": "f249e44df3044e7b0d665b550569ddf0",
+    "url": "/Portfolio/img/fa-brands-400.f249e44d.svg"
+  },
+  {
+    "revision": "fdc155d57b7351ef85b3028ea3cfc048",
+    "url": "/Portfolio/img/fa-solid-900.fdc155d5.svg"
+  },
+  {
+    "revision": "0986fb9a2f5e293c23b9d391dbc85430",
+    "url": "/Portfolio/img/SurveyKu.0986fb9a.png"
+  },
+  {
+    "revision": "1cfae6c3b83979a4f313e3ee213bcebb",
+    "url": "/Portfolio/img/Multis.1cfae6c3.jpg"
+  },
+  {
+    "revision": "71d6e91f0e1dc7a19fe115ad94d62f63",
+    "url": "/Portfolio/img/NUS.71d6e91f.png"
+  },
+  {
+    "revision": "1dac8e47db6f6d3f404afdab36b32233",
+    "url": "/Portfolio/img/chainstack.1dac8e47.png"
+  },
+  {
+    "revision": "79e987b717336ed7d5fba9f48af07e1e",
+    "url": "/Portfolio/img/SuperSecretChat.79e987b7.png"
+  },
+  {
+    "revision": "b5cf88c8dfaa88a5b54205eabf581e45",
+    "url": "/Portfolio/img/WorldResume.b5cf88c8.png"
+  },
+  {
+    "revision": "a8ad87308f823309518e69dc62b8f060",
+    "url": "/Portfolio/fonts/materialdesignicons-webfont.a8ad8730.woff2"
+  },
+  {
+    "revision": "31174c6f35fcbd9772533e2bf9241c58",
+    "url": "/Portfolio/fonts/materialdesignicons-webfont.31174c6f.woff"
+  },
+  {
+    "revision": "9262ae17dbf8ff24e193f88c767d6c4f",
+    "url": "/Portfolio/fonts/materialdesignicons-webfont.9262ae17.eot"
+  },
+  {
+    "revision": "f83465070f9a48c6728e6a74417141b0",
+    "url": "/Portfolio/fonts/materialdesignicons-webfont.f8346507.ttf"
+  },
+  {
+    "revision": "d830342eacd6a40ba7789628167d80d6",
+    "url": "/Portfolio/img/QuorumBox.d830342e.png"
+  },
+  {
+    "revision": "1327ee827141bb0be548da37c26cb340",
+    "url": "/Portfolio/img/2.1327ee82.png"
+  },
+  {
+    "revision": "1d25cf95b32fb9c4f271",
+    "url": "/Portfolio/js/app.6d9d4e94.js"
+  },
+  {
+    "revision": "ff3a23b6d0b1acae53e0",
+    "url": "/Portfolio/js/chunk-vendors.28acfc5b.js"
+  },
+  {
+    "revision": "6f62c361d4291d09f2c10d88b6066e51",
+    "url": "/Portfolio/img/profile.6f62c361.png"
+  },
+  {
+    "revision": "d26871e8149b5759f814fd3c7a4f784b",
+    "url": "/Portfolio/fonts/Roboto-Light.d26871e8.woff2"
+  },
+  {
+    "revision": "ad538a69b0e8615ed0419c4529344ffc",
+    "url": "/Portfolio/fonts/Roboto-Thin.ad538a69.woff2"
+  },
+  {
+    "revision": "90d1676003d9c28c04994c18bfd8b558",
+    "url": "/Portfolio/fonts/Roboto-Medium.90d16760.woff2"
+  },
+  {
+    "revision": "73f0a88bbca1bec19fb1303c689d04c6",
+    "url": "/Portfolio/fonts/Roboto-Regular.73f0a88b.woff2"
+  },
+  {
+    "revision": "b52fac2bb93c5858f3f2675e4b52e1de",
+    "url": "/Portfolio/fonts/Roboto-Bold.b52fac2b.woff2"
+  },
+  {
+    "revision": "59eb3601394dd87f30f82433fb39dd94",
+    "url": "/Portfolio/fonts/Roboto-Black.59eb3601.woff2"
+  },
+  {
+    "revision": "5b4a33e176ff736a74f0ca2dd9e6b396",
+    "url": "/Portfolio/fonts/Roboto-ThinItalic.5b4a33e1.woff2"
+  },
+  {
+    "revision": "e8eaae902c3a4dacb9a5062667e10576",
+    "url": "/Portfolio/fonts/Roboto-LightItalic.e8eaae90.woff2"
+  },
+  {
+    "revision": "4357beb823a5f8d65c260f045d9e019a",
+    "url": "/Portfolio/fonts/Roboto-RegularItalic.4357beb8.woff2"
+  },
+  {
+    "revision": "13ec0eb5bdb821ff4930237d7c9f943f",
+    "url": "/Portfolio/fonts/Roboto-MediumItalic.13ec0eb5.woff2"
+  },
+  {
+    "revision": "94008e69aaf05da75c0bbf8f8bb0db41",
+    "url": "/Portfolio/fonts/Roboto-BoldItalic.94008e69.woff2"
+  },
+  {
+    "revision": "f75569f8a5fab0893fa712d8c0d9c3fe",
+    "url": "/Portfolio/fonts/Roboto-BlackItalic.f75569f8.woff2"
+  },
+  {
+    "revision": "d3b47375afd904983d9be8d6e239a949",
+    "url": "/Portfolio/fonts/Roboto-Thin.d3b47375.woff"
+  },
+  {
+    "revision": "c73eb1ceba3321a80a0aff13ad373cb4",
+    "url": "/Portfolio/fonts/Roboto-Light.c73eb1ce.woff"
+  },
+  {
+    "revision": "35b07eb2f8711ae08d1f58c043880930",
+    "url": "/Portfolio/fonts/Roboto-Regular.35b07eb2.woff"
+  },
+  {
+    "revision": "1d6594826615607f6dc860bb49258acb",
+    "url": "/Portfolio/fonts/Roboto-Medium.1d659482.woff"
+  },
+  {
+    "revision": "50d75e48e0a3ddab1dd15d6bfb9d3700",
+    "url": "/Portfolio/fonts/Roboto-Bold.50d75e48.woff"
+  },
+  {
+    "revision": "313a65630d341645c13e4f2a0364381d",
+    "url": "/Portfolio/fonts/Roboto-Black.313a6563.woff"
+  },
+  {
+    "revision": "8a96edbbcd9a6991d79371aed0b0288e",
+    "url": "/Portfolio/fonts/Roboto-ThinItalic.8a96edbb.woff"
+  },
+  {
+    "revision": "13efe6cbc10b97144a28310ebdeda594",
+    "url": "/Portfolio/fonts/Roboto-LightItalic.13efe6cb.woff"
+  },
+  {
+    "revision": "f5902d5ef961717ed263902fc429e6ae",
+    "url": "/Portfolio/fonts/Roboto-RegularItalic.f5902d5e.woff"
+  },
+  {
+    "revision": "83e114c316fcc3f23f524ec3e1c65984",
+    "url": "/Portfolio/fonts/Roboto-MediumItalic.83e114c3.woff"
+  },
+  {
+    "revision": "4fe0f73cc919ba2b7a3c36e4540d725c",
+    "url": "/Portfolio/fonts/Roboto-BoldItalic.4fe0f73c.woff"
+  },
+  {
+    "revision": "cc2fadc3928f2f223418887111947b40",
+    "url": "/Portfolio/fonts/Roboto-BlackItalic.cc2fadc3.woff"
+  },
+  {
+    "revision": "b0c3d9de85d6b757f80bbb73d07b0d95",
+    "url": "/Portfolio/img/talenta.b0c3d9de.png"
+  },
+  {
+    "revision": "9fa80dc32a2f2580f3af6884703431ad",
+    "url": "/Portfolio/img/coinmonks.9fa80dc3.png"
+  },
+  {
+    "revision": "cb5f93192171b14aa669d2bb09095389",
+    "url": "/Portfolio/img/hackernoon.cb5f9319.png"
+  },
+  {
+    "revision": "39509a9eb6c8c603a8276a7743f4a52c",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "1d25cf95b32fb9c4f271",
+    "url": "/Portfolio/css/app.1a84574c.css"
+  }
+];
